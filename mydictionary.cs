57567d9dbaa448336a6670f9bcdf6cac137a5efa
kadir03debug <70391809+kadir03debug@users.noreply.github.com>
2021-01-23 15:47:32 +0300

    Add files via upload
    23.01.2021

---
+++ mydictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class mydictionary<T, V>  {
+    V []value;
+       T [] keys;
+        public mydictionary ()
+        {
+            value = new V [0];
+            keys = new T [0];
+        }
+       V []temp_value;
+        T []temp_keys;
+       public void Add(V val,T ke) { temp_value = value; temp_keys = keys; keys = new T[keys.Length + 1]; value = new V [value.Length + 1];for (int i = 0; i < value.Length; i++) { for (int j = 0; j < keys.Length; j++) {     value[i] = temp_value[i]; keys[j] = temp_keys[j]; } }value[value.Length - 1] = val;keys[keys.Length - 1] = ke; }
+
+
+
+    }
+}
